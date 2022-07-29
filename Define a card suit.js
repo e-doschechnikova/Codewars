64da66ecbdd979552e92482ea7e7b02ae535af6a
e-doschechnikova <evadrobakova@gmail.com>
2022-07-29 12:21:25 +0300

    fix task

--- Define a card suit.js
+++ Define a card suit.js
@@ -7,3 +7,17 @@
 // ('3♥') -> return 'hearts'
 // ('3♠') -> return 'spades'
 
+function defineSuit(card) {
+  const suit = card.substr(-1);
+
+  switch (suit) {
+    case "♣":
+      return "clubs";
+    case "♦":
+      return "diamonds";
+    case "♥":
+      return "hearts";
+    case "♠":
+      return "spades";
+  }
+}
